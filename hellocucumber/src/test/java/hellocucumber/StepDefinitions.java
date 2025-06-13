1672dbbc385be96cefa6e54936c015742cc91293
Brian Onyango <111913340+Bigboss148@users.noreply.github.com>
2025-06-13 14:54:46 -0500

    final commit

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -12,6 +12,25 @@ class IsItFriday {
 }
 
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
+    }
+
 
     @Given("I am on the home page")
     public void i_am_on_the_home_page() {
